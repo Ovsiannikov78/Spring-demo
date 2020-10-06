1f2b6e847b5c4d01642d2c08954f4da00ea5715a
Ovsiannikov78 <seri_tanya@mail.ru>
2020-10-06 17:39:37 +0200

    06_10_2020 Part - Java configuration with Java Code (without XML !)

--- spring_demo_annotations/src/main/java/de/ovsiannikov/springdemo/KickboxingCoach.java
+++ spring_demo_annotations/src/main/java/de/ovsiannikov/springdemo/KickboxingCoach.java
@@ -1,15 +1,20 @@
 package de.ovsiannikov.springdemo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
 
-@Component
 public class KickboxingCoach implements Coach {
 
-    @Autowired
-    @Qualifier("fileFortuneService")
     private FortuneService fortuneService;
+
+    @Value("${email1}")
+    private String email1;
+
+    @Value("${teamName}")
+    private String teamName;
+
+    public KickboxingCoach(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
 
     public String getDailyWorkout() {
         return "Practice low kick 20 times";
@@ -17,5 +22,13 @@ public class KickboxingCoach implements Coach {
 
     public String getDailyFortune() {
         return fortuneService.getFortune();
+    }
+
+    public String getEmail1() {
+        return email1;
+    }
+
+    public String getTeamName() {
+        return teamName;
     }
 }
