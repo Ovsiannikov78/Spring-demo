26733e41c50f050b3615e7d3fb14cc8f5098cc7b
Ovsiannikov78 <seri_tanya@mail.ru>
2020-10-30 17:57:50 +0100

    30_10_2020 Hibernate Advanced Mappings - ManyToMany Create a Main App

--- hb-05-many-to-many/src/main/java/de/ovsiannikov/hibernate/demo/CreateCourseAndReviewsDemo.java
+++ hb-05-many-to-many/src/main/java/de/ovsiannikov/hibernate/demo/CreateCourseAndReviewsDemo.java
@@ -1,15 +1,12 @@
 package de.ovsiannikov.hibernate.demo;
 
-import de.ovsiannikov.hibernate.demo.entity.Course;
-import de.ovsiannikov.hibernate.demo.entity.Instructor;
-import de.ovsiannikov.hibernate.demo.entity.InstructorDetail;
-import de.ovsiannikov.hibernate.demo.entity.Review;
+import de.ovsiannikov.hibernate.demo.entity.*;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 
-public class CreateCoursesAndReviewsDemo {
+public class CreateCourseAndReviewsDemo {
     public static void main(String[] args) {
 
         // create session factory
@@ -19,6 +16,7 @@ public class CreateCoursesAndReviewsDemo {
                 .addAnnotatedClass(InstructorDetail.class)
                 .addAnnotatedClass(Course.class)
                 .addAnnotatedClass(Review.class)
+                .addAnnotatedClass(Student.class)
                 .buildSessionFactory();
 
         // create session
@@ -31,16 +29,25 @@ public class CreateCoursesAndReviewsDemo {
             // create a course
             Course tempCourse = new Course("Spring & Hibernate - full course for beginners");
 
-            // add some reviews
-            tempCourse.addReview(new Review("Great course ... love it"));
-            tempCourse.addReview(new Review("Very useful. Thank you !!!"));
-            tempCourse.addReview(new Review("Good job! Well done!"));
 
-            // save the course ... and leverage the cascade all
-            System.out.println("Saving the course");
-            System.out.println(tempCourse);
-            System.out.println(tempCourse.getReviews());
+            // save the course
+            System.out.println("\nSaving the course...");
             session.save(tempCourse);
+            System.out.println("Saved the course: " + tempCourse);
+
+            // create the student
+            Student theStudent1 = new Student("Alex", "Bulkin", "alex@gmail.com");
+            Student theStudent2 = new Student("Jack", "Gibson", "g.gibson@gmail.com");
+
+            // add students to the course
+            tempCourse.addStudent(theStudent1);
+            tempCourse.addStudent(theStudent2);
+
+            // save the students
+            System.out.println("\nSaving the students...");
+            session.save(theStudent1);
+            session.save(theStudent2);
+            System.out.println("Saved the students: " + tempCourse.getStudents());
 
             // commit transaction
             session.getTransaction().commit();
