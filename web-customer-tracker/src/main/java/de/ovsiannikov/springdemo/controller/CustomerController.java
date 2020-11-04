9dec33367427224b731434a244c36ec6527bf58e
Ovsiannikov78 <seri_tanya@mail.ru>
2020-11-04 19:44:15 +0100

    04_11_2020_Build a Database Web App - Spring MVC and Hibernate Project - Part 2 Creating Hibernate Entity, Developing Hibernate DAO, Injecting DAO into Controller

--- web-customer-tracker/src/main/java/de/ovsiannikov/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/main/java/de/ovsiannikov/springdemo/controller/CustomerController.java
@@ -1,15 +1,31 @@
 package de.ovsiannikov.springdemo.controller;
 
+import de.ovsiannikov.springdemo.dao.CustomerDAO;
+import de.ovsiannikov.springdemo.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+    // need to inject the customer dao
+    @Autowired
+    private CustomerDAO customerDAO;
+
     @RequestMapping("/list")
     public String listCustomer(Model theModel) {
+
+        // get customer from the dao
+        List<Customer> theCustomers = customerDAO.getCustomers();
+
+        // add the customer to the model
+        theModel.addAttribute("customer", theCustomers);
+
         return "list-customers";
     }
 }
