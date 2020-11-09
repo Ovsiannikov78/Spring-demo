8556090014682e93a0c69cdbeb3307166928c959
Ovsiannikov78 <seri_tanya@mail.ru>
2020-11-09 19:01:02 +0100

    09_11_2020_Build a Database Web App - Spring MVC and Hibernate Project - Part 4 Add Customer - Sort Customer Data by first name

--- web-customer-tracker/src/main/java/de/ovsiannikov/springdemo/dao/CustomerDAOImpl.java
+++ web-customer-tracker/src/main/java/de/ovsiannikov/springdemo/dao/CustomerDAOImpl.java
@@ -21,8 +21,9 @@ public class CustomerDAOImpl implements CustomerDAO {
         // get the current hibernate session
         Session currentSession = sessionFactory.getCurrentSession();
 
-        // create a query
-        Query<Customer> theQuery = currentSession.createQuery("from Customer ", Customer.class);
+        // create a query ... sort by first name
+        Query<Customer> theQuery = currentSession.createQuery("from Customer order by firstName",
+                Customer.class);
 
         // execute query and get result list
         List<Customer> customers = theQuery.getResultList();
