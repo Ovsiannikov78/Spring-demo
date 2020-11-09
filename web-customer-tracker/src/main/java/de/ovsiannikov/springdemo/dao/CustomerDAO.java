70e0f230dca5ec6bbd23fedfed5a4e3c5a41eede
Ovsiannikov78 <seri_tanya@mail.ru>
2020-11-09 18:32:37 +0100

    09_11_2020_Build a Database Web App - Spring MVC and Hibernate Project - Part 4 Add Customer - Save added to the form customer to Database !!!

--- web-customer-tracker/src/main/java/de/ovsiannikov/springdemo/dao/CustomerDAO.java
+++ web-customer-tracker/src/main/java/de/ovsiannikov/springdemo/dao/CustomerDAO.java
@@ -1,9 +1,10 @@
 package de.ovsiannikov.springdemo.dao;
 
 import de.ovsiannikov.springdemo.entity.Customer;
-
 import java.util.List;
 
 public interface CustomerDAO {
-    public List<Customer> getCustomers();
+    List<Customer> getCustomers();
+
+    void saveCustomer(Customer theCustomer);
 }
