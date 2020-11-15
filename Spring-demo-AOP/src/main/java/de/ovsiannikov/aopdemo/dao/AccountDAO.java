57af4ada2c9685ab83c6d8bce38db532345a6dfe
Ovsiannikov78 <seri_tanya@mail.ru>
2020-11-15 06:39:36 +0100

    15_11_2020_AOP @Before Advice - Add and Test AOP Aspect

---
+++ Spring-demo-AOP/src/main/java/de/ovsiannikov/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,12 @@
+package de.ovsiannikov.aopdemo.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccountDAO {
+
+    public void addAccount() {
+
+        System.out.println(getClass() + ": DOING MY DB WORK: ADDING AN ACCOUNT");
+    }
+}
