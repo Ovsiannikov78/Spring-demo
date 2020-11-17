947dbdee4c61eb795838cea07dfe80c0a0a23bde
Ovsiannikov78 <seri_tanya@mail.ru>
2020-11-17 11:09:32 +0100

    17_11_2020 AOP: Ordering Aspects Add @Order annotation to Aspect

--- Spring-demo-aop-pointcut-order-aspects/src/main/java/de/ovsiannikov/aopdemo/aspect/MyCloudLogAsyncAspect.java
+++ Spring-demo-aop-pointcut-order-aspects/src/main/java/de/ovsiannikov/aopdemo/aspect/MyCloudLogAsyncAspect.java
@@ -2,10 +2,12 @@ package de.ovsiannikov.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(1)
 public class MyCloudLogAsyncAspect {
     @Before("de.ovsiannikov.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
     public void logToCloudAsync() {
