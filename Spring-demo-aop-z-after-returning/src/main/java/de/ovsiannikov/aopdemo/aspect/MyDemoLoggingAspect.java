59dd1546443d1fab98d41afb8c39f4673fa98a72
Ovsiannikov78 <seri_tanya@mail.ru>
2020-11-17 18:25:41 +0100

    17_11_2020 AOP: @AfterReturning Advice Type

---
+++ Spring-demo-aop-z-after-returning/src/main/java/de/ovsiannikov/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,44 @@
+package de.ovsiannikov.aopdemo.aspect;
+
+import de.ovsiannikov.aopdemo.Account;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Order(2)
+public class MyDemoLoggingAspect {
+
+    @Before("de.ovsiannikov.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
+    public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
+
+        System.out.println("\n======>>> Executing @Before advice on all methods in a package excluding getters and setters <<<======== ");
+
+        // display the method signature
+        MethodSignature methodSig = (MethodSignature) theJoinPoint.getSignature();
+
+        System.out.println("Method: " + methodSig);
+
+        // display method arguments
+
+        // get args
+        Object[] args = theJoinPoint.getArgs();
+
+        // loop thru args
+        for (Object tempArg : args) {
+            System.out.println(tempArg);
+
+            if (tempArg instanceof Account) {
+
+                // downcast and print Account specific stuff
+                Account theAccount = (Account) tempArg;
+
+                System.out.println("Account name: " + theAccount.getName() + "\nAccount level: " + theAccount.getLevel());
+            }
+        }
+    }
+}
