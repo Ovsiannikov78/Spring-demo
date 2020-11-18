711f9110c9a8276bd2391d6496058ca323640f2c
Ovsiannikov78 <seri_tanya@mail.ru>
2020-11-18 08:55:31 +0100

    18_11_2020 AOP: @AfterReturning Advice Type Add @AfterReturning advice

--- Spring-demo-aop-z-after-returning/src/main/java/de/ovsiannikov/aopdemo/aspect/MyDemoLoggingAspect.java
+++ Spring-demo-aop-z-after-returning/src/main/java/de/ovsiannikov/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,16 +2,33 @@ package de.ovsiannikov.aopdemo.aspect;
 
 import de.ovsiannikov.aopdemo.Account;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Aspect
 @Component
 @Order(2)
 public class MyDemoLoggingAspect {
+
+    // add a new advice for @AfterReturning on the findAccount method
+    @AfterReturning(pointcut = "execution(* de.ovsiannikov.aopdemo.dao.AccountDAO.findAccount(..))",
+            returning = "result")
+    public void afterReturningFindAccountAdvice(JoinPoint theJoinPoint, List<Account> result) {
+
+        // print out which method we are advising on
+        String method = theJoinPoint.getSignature().toShortString();
+        System.out.println("\n=========>>> Executing @AfterReturning on method: " + method);
+
+        // print out the results of the method call
+        System.out.println("\n=========>>> result is: " + result);
+
+    }
 
     @Before("de.ovsiannikov.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
     public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
--- Spring-demo-aop-z-after-returning/src/main/java/de/ovsiannikov/aopdemo/dao/AccountDAO.java
+++ Spring-demo-aop-z-after-returning/src/main/java/de/ovsiannikov/aopdemo/dao/AccountDAO.java
@@ -3,11 +3,28 @@ package de.ovsiannikov.aopdemo.dao;
 import de.ovsiannikov.aopdemo.Account;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Component
 public class AccountDAO {
 
     private String name;
     private String serviceCode;
+
+    // add a new method: findAccount()
+    public List<Account> findAccount() {
+
+        // create sample accounts
+        Account temp1 = new Account("Jack", "Silver");
+        Account temp2 = new Account("Alex", "Platinum");
+        Account temp3 = new Account("Inga", "Gold");
+
+        List<Account> myAccounts = Arrays.asList(temp1, temp2, temp3);
+
+        return myAccounts;
+    }
 
     public void addAccount(Account theAccount, boolean vipFlag) {
 
