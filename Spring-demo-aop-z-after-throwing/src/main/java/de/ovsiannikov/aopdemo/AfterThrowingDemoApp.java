b6d664c8aa2d168a3c7418840be6612e9cacc7c7
Ovsiannikov78 <seri_tanya@mail.ru>
2020-11-20 06:58:19 +0100

    20_11_2020 AOP: @AfterThrowing Advice Type In Main App, add a try/catch block for exception handling

---
+++ Spring-demo-aop-z-after-throwing/src/main/java/de/ovsiannikov/aopdemo/AfterThrowingDemoApp.java
@@ -0,0 +1,37 @@
+package de.ovsiannikov.aopdemo;
+
+import de.ovsiannikov.aopdemo.dao.AccountDAO;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+public class AfterThrowingDemoApp {
+
+    public static void main(String[] args) {
+
+        // read spring config java class
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+
+        // get the bean from spring container
+        AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+
+        // call method to find the accounts
+        List<Account> theAccounts = null;
+        try {
+            theAccounts = theAccountDAO.findAccount();
+        } catch (Exception e) {
+            System.out.println("\n\nMain Program ... caught exception: " + e);
+        }
+
+        // display the accounts
+        System.out.println("\n\nMain program: AfterThrowingDemoApp");
+        System.out.println("----------------------------------------");
+
+        System.out.println(theAccounts);
+
+        System.out.println("\n");
+
+        // close the context
+        context.close();
+    }
+}
