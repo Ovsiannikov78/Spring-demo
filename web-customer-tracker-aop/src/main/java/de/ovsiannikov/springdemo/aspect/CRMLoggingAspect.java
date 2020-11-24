9b5651c332cb5b717c9c353080d0d888bf6dee2d
Ovsiannikov78 <seri_tanya@mail.ru>
2020-11-24 06:53:10 +0100

    24_11_2020 AOP: AOP and Spring MVC App Add @Before Advice Add @AfterReturning Advice

--- web-customer-tracker-aop/src/main/java/de/ovsiannikov/springdemo/aspect/CRMLoggingAspect.java
+++ web-customer-tracker-aop/src/main/java/de/ovsiannikov/springdemo/aspect/CRMLoggingAspect.java
@@ -1,13 +1,17 @@
 package de.ovsiannikov.springdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import java.util.logging.Logger;
 
 @Aspect
 @Component
+/*@Configuration*/
 public class CRMLoggingAspect {
 
     // setup logger
@@ -32,6 +36,34 @@ public class CRMLoggingAspect {
     }
 
     // add @Before advice
+    public void before(JoinPoint theJoinPoint) {
+
+        // display method we are calling
+        String theMethod = theJoinPoint.getSignature().toShortString();
+        myLogger.info("=========>>> in @Before: calling method: " + theMethod);
+
+        // display the arguments to the method
+
+        // get the arguments
+        Object[] args = theJoinPoint.getArgs();
+
+        // loop thru and display args
+        for (Object tempArg : args) {
+            myLogger.info("=========>>> argument: " + tempArg);
+        }
+    }
 
     // add @AfterReturning advice
+
+    @AfterReturning(pointcut = "forAppFlow()", returning = "theResult")
+    public void afterReturning(JoinPoint theJoinPoint, Object theResult) {
+
+        // display method we are returning from
+        String theMethod = theJoinPoint.getSignature().toShortString();
+        myLogger.info("========>>> in @AfterReturning: from method: " + theMethod);
+
+        // display data returned
+        myLogger.info("=========>>> result: " + theResult);
+
+    }
 }
