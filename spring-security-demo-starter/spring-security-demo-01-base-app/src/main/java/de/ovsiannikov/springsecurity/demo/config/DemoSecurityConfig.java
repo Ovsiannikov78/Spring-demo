b6dee9ac4168c27aca295599bf00666752eed8a2
Ovsiannikov78 <seri_tanya@mail.ru>
2020-11-25 14:34:12 +0100

    25_11_2020 Spring Security - Getting Started Create Security Config

--- spring-security-demo-starter/spring-security-demo-01-base-app/src/main/java/de/ovsiannikov/springsecurity/demo/config/DemoSecurityConfig.java
+++ spring-security-demo-starter/spring-security-demo-01-base-app/src/main/java/de/ovsiannikov/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,16 +1,24 @@
 package de.ovsiannikov.springsecurity.demo.config;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 
-@Configuration
+
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+
+        // add users for in memory authentication
+
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser(users.username("john").password("test123").roles("EMPLOYEE"))
+                .withUser(users.username("alex").password("test123").roles("MANAGER"))
+                .withUser(users.username("tatiana").password("test123").roles("ADMIN"));
     }
 }
