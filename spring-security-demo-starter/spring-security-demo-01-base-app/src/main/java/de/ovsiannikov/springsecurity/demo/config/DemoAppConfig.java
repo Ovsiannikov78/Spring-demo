c8ee03c0d947e9c02e3373665d1681c0adf45484
Ovsiannikov78 <seri_tanya@mail.ru>
2020-11-28 18:34:16 +0100

    28_11_2020 Spring Security - Add JDBC Database Authentication Define DataSource

--- spring-security-demo-starter/spring-security-demo-01-base-app/src/main/java/de/ovsiannikov/springsecurity/demo/config/DemoAppConfig.java
+++ spring-security-demo-starter/spring-security-demo-01-base-app/src/main/java/de/ovsiannikov/springsecurity/demo/config/DemoAppConfig.java
@@ -1,18 +1,34 @@
 package de.ovsiannikov.springsecurity.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.sql.DataSource;
+import java.util.logging.Logger;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "de.ovsiannikov.springsecurity.demo")
+@PropertySource("classpath:persistence-mysql.properties")
 public class DemoAppConfig implements WebMvcConfigurer {
+
+    // set up variable to hold the properties
+
+    @Autowired
+    private Environment env;
+
+    // set up a logger for diagnostics
+
+    private final Logger logger = Logger.getLogger(getClass().getName());
 
     @Bean
     public ViewResolver viewResolver() {
@@ -28,5 +44,21 @@ public class DemoAppConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+    }
+
+    @Bean
+    public DataSource securityDataSource() {
+
+        // create connection pool
+
+        // set the jdbc driver class
+
+        // log the connection props
+
+        // set database connection props
+
+        // set connection pool props
+
+        return null;
     }
 }
