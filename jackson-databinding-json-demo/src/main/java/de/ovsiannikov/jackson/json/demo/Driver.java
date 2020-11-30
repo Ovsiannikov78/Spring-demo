b465b8cec3d1657c5fad8db0e561674644ceb2bd
Ovsiannikov78 <seri_tanya@mail.ru>
2020-11-30 06:15:12 +0100

    30_11_2020 Spring REST - JSON Data Binding Nested Objects and Arrays and print them

--- jackson-databinding-json-demo/src/main/java/de/ovsiannikov/jackson/json/demo/Driver.java
+++ jackson-databinding-json-demo/src/main/java/de/ovsiannikov/jackson/json/demo/Driver.java
@@ -15,12 +15,27 @@ public class Driver {
 
         // read JSON file and map/convert to Java POJO: data/sample-lite.json
 
-            Student theStudent = mapper.readValue(new File("data/sample-lite.json"),Student.class);
+            Student theStudent = mapper.readValue(new File("data/sample-full.json"),Student.class);
 
         // print first name and last name
             System.out.println("First name = " + theStudent.getFirstName());
             System.out.println("Last name = " + theStudent.getLastName());
 
+        // print out address
+            Address tempAddress = theStudent.getAddress();
+            System.out.println("\n=======>>> Address <<<=========\n");
+
+            System.out.println("Street = " + tempAddress.getStreet());
+            System.out.println("City = " + tempAddress.getCity());
+            System.out.println("State = " + tempAddress.getState());
+            System.out.println("Zip = " + tempAddress.getZip());
+            System.out.println("Country = " + tempAddress.getCountry());
+
+        // print out languages
+            System.out.println("\n=======>>> Languages <<<=========\n");
+            for (String language : theStudent.getLanguages()) {
+                System.out.println(language);
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
