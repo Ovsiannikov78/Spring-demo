2a09e2b97044af5a3011e9b9c0e41d86e05fc7dd
Ovsiannikov78 <seri_tanya@mail.ru>
2020-12-03 07:41:44 +0100

    03_12_2020 Spring REST - Using @PathVariable for REST Endpoints Refactoring Code

--- spring-rest-demo/src/main/java/de/ovsiannikov/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/de/ovsiannikov/springdemo/rest/StudentRestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,14 +13,22 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
+    private List<Student> theStudents;
+
+    // define @PostConstructor to load the student data ... only once!
+
+    @PostConstruct
+    public void loadData() {
+
+        theStudents = Arrays.asList(new Student("Vlad", "Schneider"),
+                new Student("Jack", "Bond"),
+                new Student("Alina", "Gibson"));
+    }
+
     // define endpoint for "/student" - return list of students
 
     @GetMapping("/students")
     public List<Student> getStudent() {
-
-        List<Student> theStudents = Arrays.asList(new Student("Vlad", "Schneider"),
-                new Student("Jack", "Bond"),
-                new Student("Alina", "Gibson"));
 
         return theStudents;
     }
