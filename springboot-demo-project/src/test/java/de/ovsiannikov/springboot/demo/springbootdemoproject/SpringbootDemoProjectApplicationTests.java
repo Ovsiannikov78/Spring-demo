7ca09a51b24088de4bcf7f67605cfde854f35a0b
Ovsiannikov78 <seri_tanya@mail.ru>
2020-12-11 15:24:34 +0100

    11_12_2020 Spring Boot - Creating a Project with Spring Boot Initializr

---
+++ springboot-demo-project/src/test/java/de/ovsiannikov/springboot/demo/springbootdemoproject/SpringbootDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ovsiannikov.springboot.demo.springbootdemoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
