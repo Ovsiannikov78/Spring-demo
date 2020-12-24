c5911c483514fb7664ac2cb3aad92b667715c096
Ovsiannikov78 <seri_tanya@mail.ru>
2020-12-24 15:24:40 +0100

    24_12_2020 Spring Boot - Spring Data REST - Real-Time Project Way to expose a different resource name - specify plural name/path with an annotation @RepositoryRestResource

--- spring-data-rest-cruddemo/src/main/java/de/ovsiannikov/springboot/cruddemo/dao/EmployeeRepository.java
+++ spring-data-rest-cruddemo/src/main/java/de/ovsiannikov/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package de.ovsiannikov.springboot.cruddemo.dao;
 
 import de.ovsiannikov.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
