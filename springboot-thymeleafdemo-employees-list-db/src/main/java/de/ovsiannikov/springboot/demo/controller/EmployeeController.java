bee14432b58bf22a84f3bc98910b2d06646308ef
Ovsiannikov78 <seri_tanya@mail.ru>
2020-12-26 12:19:30 +0100

    26_12_2020 Spring Boot - Thymeleaf - CRUD Database Real-Time Project Add Employee - Part 3 - Process form data to save employee to the DB and add a method to sort date by first name

--- springboot-thymeleafdemo-employees-list-db/src/main/java/de/ovsiannikov/springboot/demo/controller/EmployeeController.java
+++ springboot-thymeleafdemo-employees-list-db/src/main/java/de/ovsiannikov/springboot/demo/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import de.ovsiannikov.springboot.demo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -41,5 +43,15 @@ public class EmployeeController {
         theModel.addAttribute("employee", theEmployee);
 
         return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+
+        // save the employee
+        employeeService.save(theEmployee);
+
+        // use a redirect to prevent duplicate submission
+        return "redirect:/employees/list";
     }
 }
--- springboot-thymeleafdemo-employees-list-db/src/main/java/de/ovsiannikov/springboot/demo/dao/EmployeeRepository.java
+++ springboot-thymeleafdemo-employees-list-db/src/main/java/de/ovsiannikov/springboot/demo/dao/EmployeeRepository.java
@@ -3,5 +3,10 @@ package de.ovsiannikov.springboot.demo.dao;
 import de.ovsiannikov.springboot.demo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // add a method to sort by first name
+    public List<Employee> findAllByOrderByFirstNameAsc();
 }
