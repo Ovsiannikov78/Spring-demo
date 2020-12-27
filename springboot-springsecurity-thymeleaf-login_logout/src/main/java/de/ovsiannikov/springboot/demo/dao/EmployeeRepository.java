2b35a812a7af7036beeeb6076e6595c2c88d613b
Ovsiannikov78 <seri_tanya@mail.ru>
2020-12-27 19:01:20 +0100

    27_12_2020 Spring Security - Login/Logout with Spring Boot and Thymeleaf - CRUD Database Real-Time Project

---
+++ springboot-springsecurity-thymeleaf-login_logout/src/main/java/de/ovsiannikov/springboot/demo/dao/EmployeeRepository.java
@@ -0,0 +1,19 @@
+package de.ovsiannikov.springboot.demo.dao;
+
+import java.util.List;
+
+import de.ovsiannikov.springboot.demo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+	// that's it ... no need to write any code LOL!
+
+	// add a method to sort by last name
+	public List<Employee> findAllByOrderByLastNameAsc();
+
+	// search by name
+	public List<Employee> findByFirstNameContainsOrLastNameContainsAllIgnoreCase(String name, String lName);
+
+}
